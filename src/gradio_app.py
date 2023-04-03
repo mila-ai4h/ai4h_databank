e5b0a098f977bff6d20ac98c6d9f2a24d349d3c9
Jeremy Pinto <jerpint@gmail.com>
2023-04-03 15:43:14 -0400

    remove data from repo, switch to datasets space

--- src/gradio_app.py
+++ src/gradio_app.py
@@ -1,12 +1,17 @@
-import cfg
 import os
+import logging
+
 import gradio as gr
 import openai
+from huggingface_hub import hf_hub_download
 
 from buster.busterbot import Buster
 from buster.retriever import Retriever
 from buster.utils import get_retriever_from_extension
+import cfg
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 # initialize buster with the config in config.py (adapt to your needs) ...
 retriever: Retriever = get_retriever_from_extension(cfg.documents_filepath)(
@@ -14,16 +19,38 @@ retriever: Retriever = get_retriever_from_extension(cfg.documents_filepath)(
 )
 buster: Buster = Buster(cfg=cfg.buster_cfg, retriever=retriever)
 
+# auth information
 USERNAME = os.getenv("AI4H_USERNAME")
 PASSWORD = os.getenv("AI4H_PASSWORD")
+
+# hf hub information
+REPO_ID = "jerpint/databank-ai4h"
+DB_FILE = "documents.db"
+HUB_TOKEN = os.environ.get("HUB_TOKEN")
 
 # set openAI creds
 openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.organization = os.getenv("OPENAI_ORGANIZATION")
 
 
+# download the documents.db hosted on the dataset space
+logger.info(f"Downloading {DB_FILE} from hub...")
+hf_hub_download(
+    repo_id=REPO_ID,
+    repo_type="dataset",
+    filename=DB_FILE,
+    token=HUB_TOKEN,
+    local_dir=".",
+)
+logger.info(f"Downloaded.")
+
+
 def check_auth(username, password):
-    return username == USERNAME and password == PASSWORD
+    """Basic auth, only supports a single user."""
+    # TODO: update to better auth
+    is_auth = username == USERNAME and password == PASSWORD
+    logger.info(f"Log-in attempted. {is_auth=}")
+    return is_auth
 
 
 def chat(question, history):
