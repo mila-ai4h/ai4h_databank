73ef6123e9827aaafb3520f7259a774748f1e761
Jeremy Pinto <jerpint@gmail.com>
2023-04-11 11:21:32 -0400

    update frontend and add new source of docs

--- src/cfg.py
+++ src/cfg.py
@@ -1,14 +1,60 @@
-from buster.busterbot import BusterConfig
+import logging
+import os
 
-documents_filepath = "./documents.db"
+import openai
+from buster.busterbot import BusterConfig
+from buster.retriever import Retriever
+from buster.utils import get_retriever_from_extension
+from huggingface_hub import hf_hub_download
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+# auth information
+USERNAME = os.getenv("AI4H_USERNAME")
+PASSWORD = os.getenv("AI4H_PASSWORD")
+
+# set openAI creds
+openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.organization = os.getenv("OPENAI_ORGANIZATION")
+
+
+# hf hub information
+REPO_ID = "jerpint/databank-ai4h"
+DB_FILE = "documents_oecd.db"
+HUB_TOKEN = os.environ.get("HUB_TOKEN")
+# download the documents.db hosted on the dataset space
+logger.info(f"Downloading {DB_FILE} from hub...")
+hf_hub_download(
+    repo_id=REPO_ID,
+    repo_type="dataset",
+    filename=DB_FILE,
+    token=HUB_TOKEN,
+    local_dir=".",
+)
+logger.info("Downloaded.")
+
+# setup retriever
+retriever: Retriever = get_retriever_from_extension(DB_FILE)(DB_FILE)
+
+
 buster_cfg = BusterConfig(
-    unknown_prompt="I'm sorry, but I am an AI language model trained to assist with questions related to AI. I cannot answer that question as it is not relevant to the library or its usage. Is there anything else I can assist you with?",
     embedding_model="text-embedding-ada-002",
-    top_k=3,
-    thresh=0.7,
-    max_words=3000,
-    completer_cfg={
+    unknown_prompt="I'm sorry, but I am an AI language model trained to assist with questions related to AI. I cannot answer that question as it is not relevant to the library or its usage. Is there anything else I can assist you with?",
+    unknown_threshold=0.85,
+    retriever_cfg={
+        "top_k": 3,
+        "thresh": 0.7,
+    },
+    completion_cfg={
         "name": "ChatGPT",
+        "completion_kwargs": {
+            "model": "gpt-3.5-turbo",
+            "temperature": 0,
+        },
+    },
+    prompt_cfg={
+        "max_words": 3000,
         "text_before_documents": (
             "You are a chatbot assistant answering technical questions about artificial intelligence (AI)."
             "You can only respond to a question if the content necessary to answer the question is contained in the following provided documentation. "
@@ -34,10 +80,8 @@ buster_cfg = BusterConfig(
             "I'm sorry, but I am an AI language model trained to assist with questions related to AI. I cannot answer that question as it is not relevant to the library or its usage. Is there anything else I can assist you with?"
             "Now answer the following question:\n"
         ),
-        "completion_kwargs": {
-            "model": "gpt-3.5-turbo",
-        },
     },
-    response_format="gradio",
-    source="stackoverflow",
+    document_source="LimeSurvey",
 )
+
+document_sources = ["LimeSurvey"]
