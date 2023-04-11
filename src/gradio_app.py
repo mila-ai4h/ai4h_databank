4db3e4b51a15aa52e1d972249ea823ce30b62f4a
Jeremy Pinto <jerpint@gmail.com>
2023-04-11 15:32:09 -0400

    lint

--- src/gradio_app.py
+++ src/gradio_app.py
@@ -75,7 +75,9 @@ with block:
             with gr.Column(scale=1):
                 gr.Markdown("#### Model")
                 # TODO: remove interactive=False flag when deployed model gets access to GPT4
-                model = gr.Radio(cfg.available_models, label="Model to use", value=cfg.available_models[0], interactive=False)
+                model = gr.Radio(
+                    cfg.available_models, label="Model to use", value=cfg.available_models[0], interactive=False
+                )
             with gr.Column(scale=1):
                 gr.Markdown("#### Sources")
                 case_names = sorted(cfg.document_sources)
