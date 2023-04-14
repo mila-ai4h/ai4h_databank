d0939bb4077ade1e398cbaa1513512ed8d5c8c52
Jeremy Pinto <jerpint@gmail.com>
2023-04-14 13:55:22 -0400

    add feedback (#16)
    * add feedback form
    
    * log feedback to mongodb
    
    * bump buster to latest version

---
+++ src/db_utils.py
@@ -0,0 +1,38 @@
+# Setup mongoDB
+import logging
+import os
+from dataclasses import dataclass
+
+from buster.busterbot import Response
+from fastapi.encoders import jsonable_encoder
+from pymongo import MongoClient
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+MONGODB_PASSWORD = os.getenv("MONGODB_AI4H_PASSWORD")
+MONGODB_USERNAME = os.getenv("MONGODB_AI4H_USERNAME")
+MONGODB_DB_NAME = os.getenv("MONGODB_AI4H_DB_NAME")
+ATLAS_URI = f"mongodb+srv://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@{MONGODB_DB_NAME}.m0zt2w2.mongodb.net/?retryWrites=true&w=majority"
+
+
+def init_db():
+    if all(v is not None for v in [MONGODB_PASSWORD, MONGODB_USERNAME, MONGODB_DB_NAME]):
+        mongodb_client = MongoClient(ATLAS_URI)
+        database = mongodb_client[MONGODB_DB_NAME]
+        logger.info("Connected to the MongoDB database!")
+
+    else:
+        database = None
+        logger.warning("Didn't connect to MongoDB database, check auth.")
+
+    return database
+
+
+@dataclass
+class Feedback:
+    good_bad: str
+    extra_info: str
+    relevant_answer: str
+    relevant_length: str
+    relevant_sources: str
--- src/gradio_app.py
+++ src/gradio_app.py
@@ -1,18 +1,32 @@
+import copy
+import json
 import logging
+import uuid
+from datetime import datetime, timezone
 
 import gradio as gr
 import pandas as pd
 from buster.busterbot import Buster
+from fastapi.encoders import jsonable_encoder
 
 import cfg
+from db_utils import Feedback, init_db
+
+mongo_db = init_db()
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-buster: Buster = Buster(cfg=cfg.buster_cfg, retriever=cfg.retriever)
-
 
 MAX_TABS = cfg.buster_cfg.retriever_cfg["top_k"]
+
+
+def get_utc_time() -> str:
+    return str(datetime.now(timezone.utc))
+
+
+def get_session_id() -> str:
+    return str(uuid.uuid1())
 
 
 def check_auth(username: str, password: str) -> bool:
@@ -38,7 +52,7 @@ def pad_sources(sources: list[str]) -> list[str]:
     return sources + [""] * (MAX_TABS - k)
 
 
-def chat(question, history, document_source, model):
+def chat(question, history, document_source, model, user_responses):
     history = history or []
 
     cfg.buster_cfg.document_source = document_source
@@ -53,12 +67,69 @@ def chat(question, history, document_source, model):
     sources = format_sources(response.matched_documents)
     sources = pad_sources(sources)
 
-    return history, history, *sources
+    user_responses.append(response)
+
+    return history, history, user_responses, *sources
+
+
+def user_responses_formatted(user_responses):
+    """Format user responses so that the matched_documents are in easily jsonable dict format."""
+    responses_copy = copy.deepcopy(user_responses)
+    for response in responses_copy:
+        # go to json and back to dict so that all int entries are now strings in a dict...
+        response.matched_documents = json.loads(
+            response.matched_documents.drop(columns=["embedding"]).to_json(orient="index")
+        )
+
+    logger.info(responses_copy)
+
+    return responses_copy
+
+
+def submit_feedback(
+    user_responses,
+    session_id,
+    feedback_good_bad,
+    feedback_relevant_length,
+    feedback_relevant_answer,
+    feedback_relevant_sources,
+    feedback_info,
+):
+    dict_responses = user_responses_formatted(user_responses)
+    user_feedback = Feedback(
+        good_bad=feedback_good_bad,
+        extra_info=feedback_info,
+        relevant_answer=feedback_relevant_answer,
+        relevant_length=feedback_relevant_length,
+        relevant_sources=feedback_relevant_sources,
+    )
+    feedback = {
+        "session_id": session_id,
+        "user_responses": dict_responses,
+        "feedback": user_feedback,
+        "time": get_utc_time(),
+    }
+    feedback_json = jsonable_encoder(feedback)
+
+    logger.info(feedback_json)
+    try:
+        mongo_db["feedback"].insert_one(feedback_json)
+        logger.info("response logged to mondogb")
+    except Exception as err:
+        logger.exception("Something went wrong logging to mongodb")
+    # update visibility for extra form
+    return {feedback_submitted_message: gr.update(visible=True)}
 
 
 block = gr.Blocks(css="#chatbot .overflow-y-auto{height:500px}")
 
 with block:
+    buster: Buster = Buster(cfg=cfg.buster_cfg, retriever=cfg.retriever)
+
+    # state variables are client-side and are reset every time a client refreshes the page
+    user_responses = gr.State([])
+    session_id = gr.State(get_session_id())
+
     with gr.Row():
         gr.Markdown("<h1><center>LLawMa 🦙: A Question-Answering Bot for your documentation</center></h1>")
 
@@ -78,6 +149,48 @@ with block:
                     "What's the state of AI in North America?",
                 ],
                 inputs=message,
+            )
+            gr.Markdown("#### Feedback form\nHelp us improve Buster!")
+            with gr.Row():
+                with gr.Row():
+                    feedback_good_bad = gr.Radio(choices=["👍", "👎"], label="How did buster do?")
+                    feedback_relevant_length = gr.Radio(
+                        choices=["Too Long", "Just Right", "Too Short"], label="How was the answer length?"
+                    )
+                    feedback_relevant_answer = gr.Slider(
+                        minimum=1, maximum=5, label="How relevant was the answer?", interactive=True, value="-", step=1
+                    )
+                    feedback_relevant_sources = gr.Slider(
+                        minimum=1,
+                        maximum=5,
+                        label="How relevant were the sources?",
+                        interactive=True,
+                        value="-",
+                        step=1,
+                    )
+                with gr.Row():
+                    feedback_info = gr.Textbox(
+                        label="Enter additional information (optional)",
+                        lines=10,
+                        placeholder="Enter more helpful information for us here...",
+                    )
+
+            submit_feedback_btn = gr.Button("Submit Feedback!")
+            with gr.Column(visible=False) as feedback_submitted_message:
+                gr.Markdown("Feedback recorded, thank you! 📝")
+
+            submit_feedback_btn.click(
+                submit_feedback,
+                inputs=[
+                    user_responses,
+                    session_id,
+                    feedback_good_bad,
+                    feedback_relevant_length,
+                    feedback_relevant_answer,
+                    feedback_relevant_sources,
+                    feedback_info,
+                ],
+                outputs=feedback_submitted_message,
             )
 
         with gr.Row():
@@ -113,8 +226,16 @@ with block:
     state = gr.State()
     agent_state = gr.State()
 
-    submit.click(chat, inputs=[message, state, source_dropdown, model], outputs=[chatbot, state, *sources_textboxes])
-    message.submit(chat, inputs=[message, state, source_dropdown, model], outputs=[chatbot, state, *sources_textboxes])
+    submit.click(
+        chat,
+        inputs=[message, state, source_dropdown, model, user_responses],
+        outputs=[chatbot, state, user_responses, *sources_textboxes],
+    )
+    message.submit(
+        chat,
+        inputs=[message, state, source_dropdown, model, user_responses],
+        outputs=[chatbot, state, user_responses, *sources_textboxes],
+    )
 
 
 block.launch(debug=True, share=False, auth=check_auth)
