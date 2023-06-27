95b14a71c3bf49e68d71f93a28e00f2ccc211d43
Jeremy Pinto <jerpint@gmail.com>
2023-06-27 13:34:19 -0400

    Recall@K evaluation (#39)
    * Add code to generate questions given documents
    
    * Add code to automatically evaluate recall@k on generated questions

---
+++ src/evaluation/generate_questions.py
@@ -0,0 +1,130 @@
+import logging
+import os
+import re
+
+import openai
+import pandas as pd
+from buster.completers import ChatGPTCompleter
+
+from src.cfg import retriever
+from src.db_utils import make_uri
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+# set openai creds
+openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.organization = None  #  os.getenv("OPENAI_ORGANIZATION")
+
+
+def split_questions(text: str):
+    """Splits a raw text containing multiple questions into separate questions.
+
+
+    Args:
+        text (str): The raw text containing multiple questions.
+
+    Returns:
+        list: A list of individual questions extracted from the input text.
+
+    Example:
+        >>> text = '1) What is your name?\n2) How old are you?\n3) Where do you live?\n'
+        >>> split_questions(text)
+        ['What is your name?', 'How old are you?', 'Where do you live?']
+
+    """
+    # This pattern matches the question number followed by ') ' and captures the rest of the line as the question text. Thanks ChatGPT
+    pattern = r"\d+\)\s+(.*)"
+    questions = re.findall(pattern, text)
+    return questions
+
+
+def generate_questions(document: str, completer):
+    """Generates questions based on the provided documents using a language model."""
+
+    prompt = """You generate questions based on the documents provided by the user.
+    Questions should be in the style of a typical person interested in AI policies.
+    They can be general, or specific, but should be answered by the documents provided.
+    Generate 3 questions, keep them short and on-topic.
+    RULES:
+    1) Avoid using terms directly from the documents.
+    2) Don't refer directly to the document, for example, do not say 'According to the document'.
+    3) Questions should be the kind of information you'd search for on a website, and generally broad in scope.
+    """
+    outputs = completer.complete(prompt, user_input=document, **completer.completion_kwargs)
+
+    # at this point, chatGPT generated questions in form 1) ... 2) ... so we split them
+    questions = split_questions(outputs)
+    print(f"{questions=}")
+    return questions
+
+
+def generate_questions_from_summary(document: str, completer):
+    """Generates a summary of a document, then generates a question using a language model."""
+
+    # summarize the document
+    prompt = "Summarize the content of the following document. Keep it high level"
+    summary = completer.complete(prompt, user_input=document, **completer.completion_kwargs)
+    print(f"{summary=}")
+
+    # generate questions
+    prompt = """You generate questions based on the documents provided by the user.
+    Questions should be in the style of a typical person interested in AI policies.
+    They can be general, or specific, but should be answered by the information provided.
+    Generate 3 questions, keep them short and on-topic."""
+    outputs = completer.complete(prompt, user_input=summary, **completer.completion_kwargs)
+    print(f"{outputs=}")
+    outputs = completer.complete(prompt, user_input=document, **completer.completion_kwargs)
+
+    # at this point, chatGPT generated questions in form 1) ... 2) ... so we split them
+    questions = split_questions(outputs)
+    return questions, summary
+
+
+def main():
+    # to declare beforehand
+    documents_file = "documents.csv"  # csv containing all chunks, or documents, to draw questions from
+    outputs_file = "questions.csv"  # Where to save the generated questions to
+    num_documents = 50  # Number of documents to use (generates 3 questions per doc)
+    model = "gpt-4"  # chatGPT model to use
+
+    if not os.path.isfile(documents_file):
+        # If the document isn't already found localoly, it will fetch it from mongodb and save a local copy
+        df = retriever.get_documents()
+        df = df.sort_values(by=["source", "title", "country", "year"])
+        df.to_csv(documents_file, index=False)
+
+    # load the df, then shuffle it
+    df = pd.read_csv(documents_file)
+    df = df.sample(frac=1, random_state=42)
+
+    # used for completions
+    completer = ChatGPTCompleter(
+        documents_formatter=None,
+        prompt_formatter=None,
+        completion_kwargs={"model": model, "stream": False, "temperature": 0},
+    )
+
+    results = {
+        "document": [],
+        "question": [],
+        # 'summary': [],
+    }
+
+    for idx in range(num_documents):
+        # get a document
+        document = df.iloc[idx].content
+        print(f"{document=}")
+
+        questions = generate_questions(document, completer)
+
+        for question in questions:
+            results["document"].append(document)
+            results["question"].append(question)
+
+    results_df = pd.DataFrame(results)
+    results_df.to_csv(outputs_file, index=False)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/evaluation/recall_at_k.py
@@ -0,0 +1,99 @@
+import json
+
+import pandas as pd
+from buster.retriever import Retriever
+from tqdm import tqdm
+
+from src.cfg import retriever
+
+# get access to pandas tqdm progress bar
+tqdm.pandas()
+
+
+def recall_at_k(df, k: int, column: str) -> float:
+    """Computes the average recall at K over all samples in the given DataFrame.
+
+    Args:
+        df (DataFrame): The DataFrame containing the data.
+        k (int): The cutoff rank value for computing recall.
+        column (str): The name of the column in the DataFrame that represents the document ranks.
+
+    Returns:
+        float: The average recall at K over all samples.
+
+    Assumes:
+        - Each retrieval has only one positive document.
+    """
+    return sum(df[column] <= k) / len(df)
+
+
+def get_document_rank(x, top_k: int, retriever: Retriever) -> int | float:
+    """
+    Computes the rank at which a given document was retrieved.
+
+    Args:
+        x (pandas.Series): A pandas row containing a question and document to be retrieved.
+        top_k (int): The total number of matched documents to retrieve.
+        retriever (Retriever): The Retriever object used to retrieve documents.
+
+    Returns:
+        int | float: The rank at which the document was retrieved. If the document is not found
+                     in the retrieved top_k documents, it is considered as infinity (inf).
+    """
+
+    # retrieve the top_k matched documents
+    matched_documents = retriever.get_topk_documents(x.question, source=None, top_k=top_k)
+
+    # find rank of known document
+    if any(contains_target := (matched_documents.content == x.document)):
+        # Find the first index where the value is True
+        rank = contains_target.idxmax()
+    else:
+        rank = float("inf")
+    return rank
+
+
+def main():
+    """Compute Recall@K and rank statistics for a set of questions.
+
+    This function reads questions from a CSV file generated using the 'generate_questions.py' script.
+    It retrieves a maximum of 'top_k' documents for each question, computes the rank of the retrieved document,
+    saves the results to a CSV file, and prints the rank frequency and Recall@K results.
+    """
+
+    # args
+    questions_csv = "questions.csv"  # generated questions using the generate_questions.py script
+    top_k = 50  # max number of documents to retrieve
+    rank_results_file = "ranking_results.csv"
+    recall_results_file = "recall_at_k_results.json"  # save results after computation
+
+    # load the questions
+    df = pd.read_csv(questions_csv)
+    print(f"Computing Recall@K for {questions_csv}. Found {len(df)} questions.")
+
+    # Computes the retrieved document's rank.
+    # progress_apply replaces apply with a pandas tqdm wrapper
+    doc_rank_column = "doc_rank"
+    df[doc_rank_column] = df.progress_apply(get_document_rank, args=(top_k, retriever), axis=1)
+
+    # Display the results
+    print(df)
+
+    # save the result to .csv
+    df.to_csv(rank_results_file, index=False)
+
+    # Compute and display the frequency of a rank
+    rank_counts = df[doc_rank_column].value_counts().sort_index()
+    print(rank_counts)
+
+    # compute the recall at k results
+    recall_at_k_results = {k: recall_at_k(df, k, column=doc_rank_column) for k in range(top_k)}
+    print(f"{recall_at_k_results}")
+
+    # Dump the results to a .json file
+    with open(recall_results_file, "w") as file:
+        json.dump(recall_at_k_results, file)
+
+
+if __name__ == "__main__":
+    main()
