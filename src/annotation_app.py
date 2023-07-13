ec939d0508b4d4edf6cfca808b5df78a1101ea26
Jeremy Pinto <jerpint@gmail.com>
2023-07-13 14:48:23 -0400

    add saving ability

--- src/annotation_app.py
+++ src/annotation_app.py
@@ -18,14 +18,21 @@ relevant_questions = questions[questions.question_type == "relevant"].question.t
 
 annotation_app = gr.Blocks()
 
+top_k = 10
+empty_documents = [""] * top_k
+empty_evaluations = [False] * top_k
+
 
 @lru_cache
 def get_relevant_documents(
     query: str,
     top_k=10,
 ) -> pd.DataFrame:
-    matched_documents = retriever.retrieve(query=query, top_k=top_k)
 
+    if query == "":
+        return empty_documents
+
+    matched_documents = retriever.retrieve(query=query, top_k=top_k)
     return matched_documents.content.to_list()
 
 
@@ -42,24 +49,24 @@ with annotation_app:
     gr.Markdown("<h1><center>Reference Annotation</center></h1>")
 
     with gr.Column(scale=2):
-        message = gr.Textbox(
+        question_input = gr.Textbox(
             label="Ask your question",
             placeholder="Ask your question here...",
             lines=1,
         )
-        ask_button = gr.Button(value="Ask", variant="secondary").style(full_width=False)
+        ask_button = gr.Button(value="Ask", variant="primary")
     with gr.Column(variant="panel"):
         gr.Markdown("## Example questions")
         with gr.Tab("Relevant questions"):
             gr.Examples(
                 examples=relevant_questions,
-                inputs=message,
+                inputs=question_input,
                 label="Questions users could ask.",
+                # fn=ask_button.click,
+                # run_on_click=True,
             )
 
-    def update_documents(*args):
-        documents = args[0]
-        document_content = args[1:]
+    def update_documents(documents):
         updated_document_content = []
 
         for doc in documents:
@@ -68,20 +75,50 @@ with annotation_app:
         return updated_document_content
 
     # keep track of submission form components here...
-    top_k = 10
-    documents = gr.State([""] * top_k)
-    document_relevance = []
+    documents = gr.State(empty_documents)
+    user_evaluations = gr.State({})
+
+    document_evaluation = []
     document_content = []
 
     with gr.Row():
         with gr.Column():
+            save_button = gr.Button(value="Save 💾", variant="primary")
             for idx in range(len(documents.value)):
                 with gr.Column():
-                    document_relevance.append(gr.Checkbox(value=False, label="relevant", interactive=True))
+                    document_evaluation.append(gr.Checkbox(value=False, label="relevant", interactive=True))
                     document_content.append(
                         gr.Textbox(label=f"Document", interactive=False, value=documents.value[idx])
                     )
 
-    ask_button.click(fn=get_relevant_documents, inputs=message, outputs=documents).then(
-        update_documents, inputs=[documents, *document_content], outputs=document_content
+    def update_evaluations(question: str, user_evaluations):
+        latest_evaluation = user_evaluations.get(question)
+        if latest_evaluation is None:
+            # return an empty evaluation
+            print("Here")
+            return empty_evaluations
+        else:
+            print("Here 1")
+
+            return latest_evaluation
+
+    def save_evaluations(question, user_evaluations, *current_evaluations):
+        print(current_evaluations)
+        user_evaluations[question] = list(current_evaluations)
+
+        return user_evaluations
+
+    # fmt: off
+    ask_button.click(
+        fn=get_relevant_documents, inputs=question_input, outputs=documents
+    ).then(
+        update_documents, inputs=[documents], outputs=document_content
+    ).then(
+        update_evaluations, inputs=[question_input, user_evaluations], outputs=document_evaluation
     )
+
+
+    save_button.click(
+        fn=save_evaluations, inputs=[question_input, user_evaluations, *document_evaluation], outputs=user_evaluations
+    )
+    # fmt: on
