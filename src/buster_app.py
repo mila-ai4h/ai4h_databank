f07847fdbe92c59e26b93a297c07c6dc3e213923
Jeremy Pinto <jerpint@gmail.com>
2023-08-16 10:15:12 -0400

    Update frontend, add explainer for feedback form

--- src/buster_app.py
+++ src/buster_app.py
@@ -33,19 +33,32 @@ relevant_questions = questions[questions.question_type == "relevant"].question.t
 irrelevant_questions = questions[questions.question_type == "irrelevant"].question.to_list()
 trick_questions = questions[questions.question_type == "trick"].question.to_list()
 
+
 def get_metadata_markdown(df):
     metadata = []
+
+    def to_link(title: str, link: str):
+        return f"[{title}]({link})"
+
     for _, item in df.iterrows():
-        metadata.append(" | ".join([str(i) for i in item]))
+        # metadata.append(" | ".join([str(i) for i in item]))
+
+        source = item["Source"]
+        link = item["Link"]
+        title = item["Title"]  # limit to 50 chars
+        year = item["Year"]
+
+        metadata.append(f"{source} | {year} | {to_link(title, link)} ")
     metadata_str = "\n".join(metadata)
 
     markdown_text = f"""
 ## 📚 Sources
-| Source | Report | Year | Link |
-| ---    | ---    | --- | --- |
+| Source | Year | Report |
+| ---    | ---    | --- |
 {metadata_str}
 """
     return markdown_text
+
 
 def append_completion(completion, user_completions):
     user_completions.append(completion)
@@ -131,35 +144,37 @@ with buster_app:
     app_name = "LLaWma 🦙"
     gr.Markdown(f"<h1><center>{app_name}: A Question-Answering Bot for your documentation</center></h1>")
     with gr.Row(variant="panel"):
-        gr.Markdown(
-            f"""
+        with gr.Box():
+            gr.Markdown(
+                f"""
 
-        ## Welcome!
+            ## Welcome!
 
-        {app_name} is connected to AI policies from various sources. Using this platform, you can ask AI-policy questions and {app_name} will look for the most relevant policies to formulate an answer based on the sources.
+            {app_name} is connected to AI policies from various sources. Using this platform, you can ask AI-policy questions and {app_name} will look for the most relevant policies to formulate an answer based on the sources.
 
-        ## How it works
-        This app uses language models to convert documents to their semanatic representations.
-        When a user asks a question, {app_name} compares it against all available documents. It then retrieves the documents that are most relevance to the question and prompts ChatGPT with those documents to generate a response.
-        The answer and accompanying sources are then displayed to the user.
-        """
-        )
+            ## How it works
+            This app uses language models to convert documents to their semanatic representations.
+            When a user asks a question, {app_name} compares it against all available documents. It then retrieves the documents that are most relevance to the question and prompts ChatGPT with those documents to generate a response.
+            The answer and accompanying sources are then displayed to the user.
+            """
+            )
 
-        gr.Markdown(
-            f"""
-        ## Limitations
+        with gr.Box():
+            gr.Markdown(
+                f"""
+            ## Limitations
 
-        {app_name} is intended to ***_only be used as a demo._*** While we have worked hard to make this as useful as possible, it is important to understand that there are no guarantees regarding the accuracy of its responses.
-        Like all language models, {app_name} might generate information that is not entirely reliable. To mitigate this, users are strongly advised to independently verify the information provided by the tool.
-        All sources available to the model are listed below.
+            {app_name} is intended to ***_only be used as a demo._*** While we have worked hard to make this as useful as possible, it is important to understand that there are no guarantees regarding the accuracy of its responses.
+            Like all language models, {app_name} might generate information that is not entirely reliable. To mitigate this, users are strongly advised to independently verify the information provided by the tool.
+            All sources available to the model are listed below.
 
-        ## Recommended usage
+            ## Recommended usage
 
-        For optimal results, employ {app_name} in scenarios where the answers to questions are explicitly present within the provided documentation.
-        It is most effective for queries that require direct extraction of information. However, for questions that demand complex reasoning spanning across an entire document or require contextual understanding, the performance of {app_name} might be limited. In such cases, alternative methods of information retrieval and analysis might be more appropriate.
+            For optimal results, employ {app_name} in scenarios where the answers to questions are explicitly present within the provided documentation.
+            It is most effective for queries that require direct extraction of information. However, for questions that demand complex reasoning spanning across an entire document or require contextual understanding, the performance of {app_name} might be limited. In such cases, alternative methods of information retrieval and analysis might be more appropriate.
 
-        """
-        )
+            """
+            )
 
     with gr.Row():
         with gr.Column(scale=2, variant="panel"):
@@ -215,25 +230,55 @@ with buster_app:
                 gr.HighlightedText(value=metadata, label="Parameters")
 
     # Feedback
-    with gr.Column(variant="panel"):
-        gr.Markdown("## Feedback form\nHelp us improve LLawMa 🦙!")
-        gr.Markdown("We would love to hear from you, use this feedback form to let us know how we did.")
+    with gr.Box():
         with gr.Row():
-            feedback_relevant_sources = gr.Radio(
-                choices=["👍", "👎"], label="Were any of the retrieved sources relevant?"
-            )
-        with gr.Row():
-            feedback_relevant_answer = gr.Radio(choices=["👍", "👎"], label="Was the generated answer useful?")
+            with gr.Box():
+                with gr.Column():
+                    gr.Markdown("## Feedback Form")
+                    with gr.Row():
+                        feedback_relevant_sources = gr.Radio(
+                            choices=["👍", "👎"], label="Were any of the retrieved sources relevant?"
+                        )
+                    with gr.Row():
+                        feedback_relevant_answer = gr.Radio(
+                            choices=["👍", "👎"], label="Was the generated answer useful?"
+                        )
 
-        feedback_info = gr.Textbox(
-            label="Enter additional information (optional)",
-            lines=10,
-            placeholder="Enter more helpful information for us here...",
-        )
+                    feedback_info = gr.Textbox(
+                        label="Enter additional information (optional)",
+                        lines=10,
+                        placeholder="Enter more helpful information for us here...",
+                    )
 
-        submit_feedback_btn = gr.Button("Submit Feedback!")
-        with gr.Column(visible=False) as feedback_submitted_message:
-            gr.Markdown("Feedback recorded, thank you! 📝")
+                    submit_feedback_btn = gr.Button("Submit Feedback!")
+                    with gr.Column(visible=False) as feedback_submitted_message:
+                        gr.Markdown("Feedback recorded, thank you! 📝")
+            with gr.Box():
+                with gr.Column():
+                    gr.Markdown(
+                        f"""## Help Us Improve with Your Feedback! 👍👎
+By filling out the feedback form, you're helping us understand what's working well and where we can make improvements on {app_name}.
+
+Rest assured, your feedback is completely anonymous, and we don't collect any personal information. This means you can express your thoughts openly and honestly, allowing us to gain valuable insights into how we can enhance the chatbot's performance and accuracy.
+
+Every thumbs up or thumbs down helps us determine how to make the chatbot better. Thank you for contributing to the evolution of our app.
+
+"""
+                    )
+
+    # TODO: Pick how to display the sources, 2 options for now
+    # Display the sources using a dataframe...
+    # gr.DataFrame(documents_metadata, headers=list(documents_metadata.columns), interactive=False)
+
+    with gr.Box():
+        with gr.Column():
+            # ... Or display the sources using markdown.
+            gr.Markdown(get_metadata_markdown(documents_metadata))
+
+            gr.HTML("<center> Powered by <a href='https://github.com/jerpint/buster'>Buster</a> 🤖</center>")
+
+    # store the users' last completion here
+    completion = gr.State()
 
     # fmt: off
     submit_feedback_btn.click(
@@ -256,18 +301,6 @@ with buster_app:
     # If you rage click the subimt feedback button, it re-appears so you are confident it was recorded properly.
     # fmt: on
 
-    # TODO: Pick how to display the sources, 2 options for now
-    # Display the sources using a dataframe...
-    # gr.DataFrame(documents_metadata, headers=list(documents_metadata.columns), interactive=False)
-
-    # ... Or display the sources using markdown.
-    gr.Markdown(get_metadata_markdown(documents_metadata))
-
-    gr.Markdown("This application uses GPT to search the docs for relevant info and answer questions.")
-
-    gr.HTML("<center> Powered by <a href='https://github.com/jerpint/buster'>Buster</a> 🤖</center>")
-
-    completion = gr.State()
 
     # fmt: off
     submit.click(
