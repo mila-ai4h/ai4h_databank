a196c7dea22f01827372382d7eaf78235b99e2f9
Jeremy Pinto <jerpint@gmail.com>
2023-09-20 10:40:03 -0400

    remove todo; default open the table

--- src/buster_app.py
+++ src/buster_app.py
@@ -443,10 +443,9 @@ def setup_additional_sources():
         Below we list all of the sources that {app_name} has access to.
         """
         )
-        with gr.Accordion(open=False, label="Click to list all available sources 📚"):
+        with gr.Accordion(open=True, label="Click to list all available sources 📚"):
             with gr.Column():
-                # TODO: Pick how to display the sources, 2 options for now
-                # Display the sources using a dataframe (rendering options limited)...
+                # Display the sources using a dataframe table
                 documents_metadata["Report"] = documents_metadata.apply(
                     lambda row: to_md_link(row["Title"], row["Link"]), axis=1
                 )
@@ -455,7 +454,7 @@ def setup_additional_sources():
                     sub_df, headers=list(sub_df.columns), interactive=False, datatype=["number", "str", "markdown"]
                 )
 
-                # ... Or display the sources using markdown.
+                # Uncomment to display the sources instead as a simple markdown table
                 # gr.Markdown(get_metadata_markdown(documents_metadata))
 
 
