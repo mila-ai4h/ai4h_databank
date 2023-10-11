4a9478a191126216336e5d976ec369e603df67e6
Jeremy Pinto <jerpint@gmail.com>
2023-10-11 15:54:41 -0400

    Update default concurrency count to 32

--- src/buster/gradio_app.py
+++ src/buster/gradio_app.py
@@ -3,7 +3,7 @@ import os
 from src.buster.buster_app import buster_app
 
 # If no value was set, use default value
-concurrent_count = int(os.getenv("CONCURRENCY_COUNT", 16))
+concurrency_count = int(os.getenv("CONCURRENCY_COUNT", 32))
 
-buster_app.queue(concurrency_count=concurrent_count)
+buster_app.queue(concurrency_count=concurrency_count)
 buster_app.launch(share=False)
