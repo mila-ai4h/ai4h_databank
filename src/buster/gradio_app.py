ac90f2fac96cc93d41d3c58f0d6440244e3345c5
Jeremy Pinto <jerpint@gmail.com>
2023-10-11 15:32:22 -0400

    pass count as int

--- src/buster/gradio_app.py
+++ src/buster/gradio_app.py
@@ -2,5 +2,8 @@ import os
 
 from src.buster.buster_app import buster_app
 
-buster_app.queue(concurrency_count=os.getenv("CONCURRENCY_COUNT", 16))
+# If no value was set, use default value
+concurrent_count = int(os.getenv("CONCURRENCY_COUNT", 16))
+
+buster_app.queue(concurrency_count=concurrent_count)
 buster_app.launch(share=False)
