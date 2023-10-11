a9243ec22346ff7fa74451dcea84f26840704575
Jeremy Pinto <jerpint@gmail.com>
2023-10-11 15:09:52 -0400

    add variable for concurrency count

--- src/buster/gradio_app.py
+++ src/buster/gradio_app.py
@@ -1,4 +1,6 @@
+import os
+
 from src.buster.buster_app import buster_app
 
-buster_app.queue(concurrency_count=16)
+buster_app.queue(concurrency_count=os.getenv("CONCURRENCY_COUNT", 16))
 buster_app.launch(share=False)
