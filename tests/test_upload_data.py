a2d525bb25950bf39938cca8f2957b03d856d62a
hbertrand <bertrand.hadrien@gmail.com>
2023-10-18 10:44:22 -0400

    chunking by token limit

---
+++ tests/test_upload_data.py
@@ -0,0 +1,28 @@
+from buster.tokenizers import GPTTokenizer
+from src.cfg import buster_cfg
+from src.upload_data import chunk_text
+
+
+def test_chunk_text():
+    tokenizer = GPTTokenizer(buster_cfg.tokenizer_cfg["model_name"])
+
+    # Test with a short text
+    text = "This is a short text."
+    token_limit = 10
+    chunks = chunk_text(text, tokenizer, token_limit)
+    assert all(tokenizer.num_tokens(chunk) <= token_limit for chunk in chunks)
+    assert "".join(chunks) == text
+
+    # Test with a long text
+    text = "This is a long text. " * 100
+    token_limit = 10
+    chunks = chunk_text(text, tokenizer, token_limit)
+    assert all(tokenizer.num_tokens(chunk) <= token_limit for chunk in chunks)
+    assert "".join(chunks) == text
+
+    # Test with a long non-latin text
+    text = "นี่คือข้อความที่ยาว. " * 100
+    token_limit = 10
+    chunks = chunk_text(text, tokenizer, token_limit)
+    assert all(tokenizer.num_tokens(chunk) <= token_limit for chunk in chunks)
+    assert "".join(chunks) == text
