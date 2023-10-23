fcce5294355392552d97f1a55ae962c04e6c7db7
Jeremy Pinto <jerpint@gmail.com>
2023-10-23 11:47:07 -0400

    fix for feedback download, add interaction download

--- src/download_feedback.py
+++ src/download_feedback.py
@@ -1,21 +1,36 @@
 import argparse
 import datetime
 
+import pandas as pd
+
+import src.cfg as cfg
 from src.app_utils import get_logging_db_name, init_db
-from src.cfg import MONGO_COLLECTION_FEEDBACK, MONGO_URI
 from src.feedback import FeedbackForm, read_collection
 
 
-def download_feedback(time: str = None):
-    mongo_db = init_db(mongo_uri=MONGO_URI, db_name=get_logging_db_name("prod"))
+def download_feedback(time: str = None) -> None:
+    mongo_db = init_db(mongo_uri=cfg.MONGO_URI, db_name=get_logging_db_name("prod"))
 
-    df = read_collection(
-        mongo_db, MONGO_COLLECTION_FEEDBACK, feedback_cls=FeedbackForm, filters={"time": {"$gt": time}}
-    )
+    filters = {"time": {"$gt": time}} if time is not None else None
+
+    df = read_collection(mongo_db, cfg.MONGO_COLLECTION_FEEDBACK, feedback_cls=FeedbackForm, filters=filters)
 
     current_date = datetime.date.today().strftime("%Y-%m-%d")
     df_name = f"feedback_{current_date}.csv"
     df.to_csv(df_name, index=False)
+
+
+def download_interactions(filters=None) -> pd.DataFrame:
+    mongo_db = init_db(mongo_uri=cfg.MONGO_URI, db_name=get_logging_db_name("prod"))
+
+    df = read_collection(
+        mongo_db,
+        cfg.MONGO_COLLECTION_INTERACTION,
+        feedback_cls=None,
+        filters=filters,
+    )
+
+    return df
 
 
 if __name__ == "__main__":
@@ -30,4 +45,4 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    collect_feedback(args.time)
+    download_feedback(args.time)
