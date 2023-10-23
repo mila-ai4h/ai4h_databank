ed7d3285dfe7b8d4b94ff689f29bae828e3d5d65
Jeremy Pinto <jerpint@gmail.com>
2023-10-23 13:03:56 -0400

    update hf_script

--- scripts/deploy_hf_space.sh
+++ scripts/deploy_hf_space.sh
@@ -57,7 +57,8 @@ prepare_deploy_dir() {
   # Copy over required files
   cp requirements.txt $TMP_DEPLOY_DIR/
   cp -r src/buster/  $TMP_DEPLOY_DIR/src/buster/
-  cp src/feedback.py src/cfg.py src/app_utils.py $TMP_DEPLOY_DIR/src/
+  cp src/cfg.py src/app_utils.py $TMP_DEPLOY_DIR/src/
+  cp -r src/feedback $TMP_DEPLOY_DIR/src/feedback
   cp data/documents_metadata.csv $TMP_DEPLOY_DIR/data
 
   # make folder cwd
