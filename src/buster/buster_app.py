a7f88c8823905030dad6a79aaef647ebce33445c
Jeremy Pinto <jerpint@gmail.com>
2023-11-01 14:15:45 -0400

    Simplify gradio code

--- src/buster/buster_app.py
+++ src/buster/buster_app.py
@@ -540,8 +540,11 @@ with buster_app:
     # fmt: on
 
     # fmt: off
-    submit.click(
-        add_user_question, [user_input], [chatbot]
+    gr.on(
+        triggers=[submit.click, user_input.submit],
+        fn=add_user_question,
+        inputs=[user_input],
+        outputs=[chatbot]
     ).then(
         clear_user_input,
         outputs=[user_input]
@@ -585,54 +588,6 @@ with buster_app:
         inputs=[last_completion, gr.State(cfg.MONGO_COLLECTION_INTERACTION), session_id]
     )
 
-    user_input.submit(
-        add_user_question, [user_input], [chatbot]
-    ).then(
-        clear_user_input,
-        outputs=[user_input]
-    ).then(
-        clear_sources,
-        outputs=[*sources_textboxes]
-    ).then(
-        toggle_visibility,
-        inputs=gr.State(False),
-        outputs=feedback_elems["submitted_message"],
-    ).then(
-        toggle_interactivity,
-        inputs=gr.State(True),
-        outputs=feedback_elems["submit_feedback_btn"],
-    ).then(
-      clear_feedback_form,
-        outputs=[
-            feedback_elems["overall_experience"],
-            feedback_elems["clear_answer"],
-            feedback_elems["accurate_answer"],
-            feedback_elems["relevant_sources"],
-            feedback_elems["relevant_sources_selection"],
-            feedback_elems["relevant_sources_order"],
-            feedback_elems["expertise"],
-            feedback_elems["extra_info"],
-        ]
-    ).then(
-        chat,
-        inputs=[chatbot],
-        outputs=[chatbot, last_completion],
-    ).then(
-        add_disclaimer,
-        inputs=[last_completion, chatbot, gr.State(cfg.disclaimer)],
-        outputs=[chatbot]
-    ).then(
-        add_sources,
-        inputs=[last_completion, gr.State(max_sources)],
-        outputs=[*sources_textboxes]
-    ).then(
-        log_completion,
-        inputs=[
-            last_completion,
-            gr.State(cfg.MONGO_COLLECTION_INTERACTION),
-            session_id,
-        ]
-    )
 
     flag_button.click(
         log_completion,
