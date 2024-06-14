cd36e8584cd178cfd979e491144d90fc2a01f407
Jeremy Pinto <jerpint@gmail.com>
2024-06-14 14:35:17 -0400

    remove debug flag

--- src/buster/gradio_app.py
+++ src/buster/gradio_app.py
@@ -4,4 +4,4 @@ from src.buster.buster_app import buster_app
 
 if __name__ == "__main__":
     buster_app.queue(api_open=False)
-    buster_app.launch(share=False, show_api=False, debug=True)
+    buster_app.launch(share=False, show_api=False)
